372699f77cb2b6e906d6c2556809bfa2dfe53fc4
Domenic <domeniczingsheim@gmail.com>
2020-10-24 20:18:14 +0200

    Doc: Add documentation to Logger namespace

--- include/DLogger/Logger.h
+++ include/DLogger/Logger.h
@@ -3,6 +3,20 @@
 
 #include <string>
 
+/**
+*	@brief This namespace capsules all Logger functions
+*	To use the logger a project name and version is to be specified. This will be used
+*	in the header of the logger.
+*	Console logging is turned on by default.
+*		To disable it call setConsoleLogging(false);
+*	File logging is turned off by default
+*		To enable it call setFileLogging(true, PATH_TO_LOG_FILE);
+*	
+*	Logging starts when calling
+*		LOGGER::start();
+*	Logging ends when calling
+*		LOGGER::end();
+*/
 namespace LOGGER
 {
 	void setProject(const std::string& name,

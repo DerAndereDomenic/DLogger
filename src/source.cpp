21b587a86cc0654ac850fb31c08c046303772cbc
Domenic <domeniczingsheim@gmail.com>
2020-10-24 20:48:24 +0200

    Add test logging messages

--- src/source.cpp
+++ src/source.cpp
@@ -6,6 +6,12 @@ int main()
 	LOGGER::setProject("DLogger Test", "1.0");
 	LOGGER::setFileLogging(true, "Log.txt");
 	LOGGER::start();
+	
+	LOGGER::DEBUG("This is a debug message\n");
+	LOGGER::INFO("This is an info message\n");
+	LOGGER::WARNING("This is a warning message\n");
+	LOGGER::ERROR("This is an error message\n");
+	
 	LOGGER::end();
 	return 0;
 }

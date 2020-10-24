da3fb82d52c938205e58edb18ae8ce8b8f71bd04
Domenic <domeniczingsheim@gmail.com>
2020-10-24 20:24:42 +0200

    Doc: Add documentation for logger functions

--- include/DLogger/Logger.h
+++ include/DLogger/Logger.h
@@ -19,16 +19,43 @@
 */
 namespace LOGGER
 {
+	/**
+	*	@brief Set project name and version.
+	*	This function has to be called before start otherwise the header will be empty
+	*	@param name The name of the project to log 
+	*	@param version The version of the project to log 
+	*/
 	void setProject(const std::string& name,
 					const std::string& version);
 					
+	/**
+	*	@brief Enables file logging.
+	*	File logging is turned off by default. To enable file logging call this with should_file_log=true 
+	*	and a valid file path
+	*	@param should_file_log If file logging should be enabled 
+	*	@param path The path to the output log file 
+	*/
 	void setFileLogging(const bool& should_file_log,
 						const std::string& path);
-						
+	
+	/**
+	*	@brief Enables console logging.
+	*	Console logging is turned on by default. To disable console logging call this with should_console_log=true
+	*	@param should_console_log If console logging should be enabled
+	*/
 	void setConsoleLogging(const bool& should_console_log);
 	
+	/**
+	*	@brief Starts logging.
+	*	After calling start() the logging functions are available and log messages
+	*/
 	void start();
 	
+	/**
+	*	@brief Ends logging.
+	*	After calling end() logging is no longer possible and the logging file will be closed if 
+	*	should_file_log=true
+	*/
 	void end();
 }
 

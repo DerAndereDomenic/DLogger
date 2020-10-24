742af908b366a991a47dcf930b03111175dd4934
Domenic <domeniczingsheim@gmail.com>
2020-10-24 18:04:03 +0200

    Initialize logger implementation

--- src/DLogger/Logger.cpp
+++ src/DLogger/Logger.cpp
@@ -1,7 +1,33 @@
 #include <DLogger/Logger.h>
 
 void 
+LOGGER::setProject(const std::string& name,
+				   const std::string& version)
+{
+	
+}
+					
+void 
+LOGGER::setFileLogging(const bool& should_file_log,
+					   const std::string& path)
+{
+	
+}
+						
+void 
+LOGGER::setConsoleLoggint(const bool& should_console_log)
+{
+	
+}
+	
+void 
 LOGGER::start()
+{
+	
+}
+	
+void 
+LOGGER::end()
 {
 	
 }

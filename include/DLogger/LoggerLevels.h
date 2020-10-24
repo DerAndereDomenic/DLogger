02e62c0bb19a46d4bfed5a21edfa0a1b5c64f3d5
Domenic <domeniczingsheim@gmail.com>
2020-10-24 20:26:40 +0200

    Doc: Add LoggerLevel documentation

--- include/DLogger/LoggerLevels.h
+++ include/DLogger/LoggerLevels.h
@@ -1,12 +1,18 @@
 #ifndef __DLOGGER_LOGGERLEVELS_H
 #define __DLOGGER_LOGGERLEVELS_H
 
+/**
+*	@brief Defines the levels of the logger.
+*	Each message has a level according to its importance.
+*	The lowest level are DEBUG messages while the highest priority is given to
+*	ERROR messages.
+*/
 enum class LoggerLevel
 {
-	DEBUG = 0,
-	INFO = 1,
-	WARNING = 2,
-	ERROR = 3
+	DEBUG = 0,		/**< The DEBUG level */
+	INFO = 1,		/**< The INFO level */
+	WARNING = 2,	/**< The WARNING level */
+	ERROR = 3		/**< The ERROR level */
 }
 
 #endif

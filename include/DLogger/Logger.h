e94d21504a6aff82e4d9cb94b8402377526fdc8a
Domenic <domeniczingsheim@gmail.com>
2020-10-24 18:02:39 +0200

    Add function definitions to initialize the logger

--- include/DLogger/Logger.h
+++ include/DLogger/Logger.h
@@ -1,9 +1,21 @@
 #ifndef __DLOGGER_LOGGER_H
 #define __DLOGGER_LOGGER_H
 
+#include <string>
+
 namespace LOGGER
 {
+	void setProject(const std::string& name,
+					const std::string& version);
+					
+	void setFileLogging(const bool& should_file_log,
+						const std::string& path);
+						
+	void setConsoleLoggint(const bool& should_console_log);
+	
 	void start();
+	
+	void end();
 }
 
 #endif
